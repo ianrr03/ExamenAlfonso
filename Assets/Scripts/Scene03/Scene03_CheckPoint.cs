bc39d87063879e64ced88e85fcca4e25d1c79471
ianrr03 <julianramierezr2003@gmail.com>
2024-03-20 20:07:47 +0100

    Escena 3 hecha falta decir con que checkpoint ha chocado

--- Assets/Scripts/Scene03/Scene03_CheckPoint.cs
+++ Assets/Scripts/Scene03/Scene03_CheckPoint.cs
@@ -2,9 +2,18 @@ using UnityEngine;
 
 public class Scene03_CheckPoint : MonoBehaviour
 {
+    [SerializeField]
+    SpriteRenderer checkPointSpriteRenderer; //para acceder al spriterenderer y poder flipear el personaje al ir para atras
+    public Color colorOriginal = Color.white; //color original spriteRenderer player
+    public Color colorAzul = Color.blue; //color dañado spriteRenderer player
 
+
+    //public GameObject ObjectToFind;
+    //string objectName = "Has chocado con el checkPoint()";
     void Start()
     {
+        //ObjectToFind = GameObject.Find(objectName);
+        //Debug.Log(ObjectToFind.name);
     }
 
 
@@ -13,21 +22,16 @@ public class Scene03_CheckPoint : MonoBehaviour
 
     }
 
-    private void OnTriggerEnter2D(Collider2D collision) //funciona el trigger de chocar, falta indicar con cual choca y cambiar el color 
+    private void OnTriggerEnter2D(Collider2D collision) //funciona el trigger de chocar y cambiar el color (falta indicar con cual checkPoint ha chocado)
     {
+
         if (collision.gameObject.CompareTag("Player")) // si algo con la etiqueta de player lo toca
         {
             Debug.Log("Has chocado con un checkPoint");
-
+            checkPointSpriteRenderer.color = colorAzul;
         }
     }
 
-    private IEnumerator CambiarColorCheckPoint() //funciona siuuuuuuuuu
-    {
-        playerSpriteRenderer.color = colorRojo;
-        yield return new WaitForSeconds(tiempoEsperaPlayerVolverColorOriginal);
-        // Volver al color original
-        playerSpriteRenderer.color = colorOriginal;
-    }
+    
 }
 
