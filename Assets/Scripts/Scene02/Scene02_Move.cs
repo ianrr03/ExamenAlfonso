d71157b4a7e1dcd4a87c74941aa280d5f5e829ee
ianrr03 <julianramierezr2003@gmail.com>
2024-03-20 19:49:15 +0100

    Commit escena 3 implementando cambio de color del checkpoint

--- Assets/Scripts/Scene02/Scene02_Move.cs
+++ Assets/Scripts/Scene02/Scene02_Move.cs
@@ -2,16 +2,34 @@ using UnityEngine;
 
 public class Scene02_Move : MonoBehaviour
 {
-    private void Start()
+    private Rigidbody2D rb2D;
+    [SerializeField]
+    private float moveSpeed;
+
+
+    private void Start() ///funciona escena 2
     {
+        this.rb2D = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
         Move();
+        MoveVertical();
     }
 
     private void Move()
     {
+        var horizontalMove = Input.GetAxisRaw("Horizontal") * this.moveSpeed;
+
+        var newVelocity = new Vector2(horizontalMove, rb2D.velocity.y);
+        this.rb2D.velocity = newVelocity;
+    }
+    private void MoveVertical()
+    {
+        var verticalMove = Input.GetAxisRaw("Vertical") * this.moveSpeed;
+
+        var newVelocity = new Vector2(rb2D.velocity.x, verticalMove);
+        this.rb2D.velocity = newVelocity;
     }
 }
--- Assets/Scripts/Scene03/Scene03_CheckPoint.cs
+++ Assets/Scripts/Scene03/Scene03_CheckPoint.cs
@@ -10,5 +10,24 @@ public class Scene03_CheckPoint : MonoBehaviour
 
     void Update()
     {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) //funciona el trigger de chocar, falta indicar con cual choca y cambiar el color 
+    {
+        if (collision.gameObject.CompareTag("Player")) // si algo con la etiqueta de player lo toca
+        {
+            Debug.Log("Has chocado con un checkPoint");
+
+        }
+    }
+
+    private IEnumerator CambiarColorCheckPoint() //funciona siuuuuuuuuu
+    {
+        playerSpriteRenderer.color = colorRojo;
+        yield return new WaitForSeconds(tiempoEsperaPlayerVolverColorOriginal);
+        // Volver al color original
+        playerSpriteRenderer.color = colorOriginal;
     }
 }
+
